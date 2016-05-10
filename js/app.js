1f1cfdd3c4798c0d20c7d398dab54ca00b177ed8
Martin Desrumaux <martin.desrumaux@epitech.eu>
2016-05-10 20:03:57 +0200

    Controle des champs + form-to-db + Woopra + Compteur firebase

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).foundation();
 
+merciPath = "/merci.html"
+
 $('.green .part-2').equalize({
   children: '.block',
   reset: true
@@ -7,4 +9,239 @@ $('.green .part-2').equalize({
 $('.equal-size').equalize({
   equalize: 'innerWidth',
   reset: true
+});
+
+// Make sure firebase API is loaded
+function counter(callbackFunction) {
+  var fb = new Firebase("https://welfarmpetitioncount.firebaseio.com");
+  fb.child('counter').transaction(function(currentValue) {
+    return (currentValue||0) + 1;
+  }, function(err, committed, snapshot){
+    if (committed){
+      //alert('ok :)');
+      Firebase.goOffline();
+      callbackFunction();
+    }
+    else {
+      //alert('Error in counter ! Maybe you are hacking me ?');
+    }
+  });
+}
+
+/* Smooth scroll */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+	scrollTop: target.offset().top + 1
+      }, 1000);
+      return false;
+    }
+  }
+  });
+});
+
+function extractUrlParams(){
+  var t = document.location.search.substring(1).split('&'); var f = [];
+  for (var i=0; i<t.length; i++){
+    var x = t[ i ].split('=');
+    f[x[0]]=decodeURIComponent(x[1]);
+  }
+  return f;
+};
+
+p = extractUrlParams();
+
+if (p['email'] && p['email'] != "undefined") {
+  $("input[name=email]").val(p['email']);
+}
+
+if (p['firstname'] && p['firstname'] != "undefined") {
+  $("input[name=firstname]").val(p['firstname']);
+}
+
+if (p['lastname'] && p['lastname'] != "undefined") {
+  $("input[name=lastname]").val(p['lastname']);
+}
+
+/*
+ * Woopra tag
+ */
+
+(function(){
+  var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+})("woopra");
+
+// TODO
+woopra.config({
+  //domain: 'spa.asso.fr',
+  //cookie_domain:'.spa.asso.fr'
+});
+
+if (p['email'] && p['email'] != "undefined") {
+  if (p['firstname'] && p['firstname'] != "undefined" &&
+      p['lastname'] && p['lastname'] != "undefined") {
+    woopra.identify({
+      email: p['email'],
+      name: p['firstname'] + ' ' + p['lastname']
+    });
+  } else {woopra.identify({email: p['email']})}
+}
+
+woopra.track();
+
+/*
+ * Debut de la lib
+*/
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+function makeCorsRequest(data) {
+  var url = 'https://form-to-db.herokuapp.com/';
+  var body = JSON.stringify(data);
+  var xhr = createCORSRequest('POST', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  // Response handlers.
+  xhr.onload = function() {
+    //var text = xhr.responseText;
+    //alert('Response from CORS request to ' + url + ': ' + text);
+    document.location.href = merciPath;
+  };
+  // Error Handler
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+  xhr.send(body);
+}
+
+/*
+ * Fin de la lib
+*/
+
+function pureField(string) {
+  return (string.replace(/'/g, "%27").replace(/"/g, "&quot;"));
+}
+
+function submitForm() {
+  var infos = {};
+  /* Si il y a un champ OPTIN sur le formulaire, et qu'il n'est pas coché,
+     l'utilisateur ne doit surement pas remonter sur threads (voir avec le chef de projet).
+     Pour effectuer ça, laisser le champ threads à undefined
+   */
+  if ($("input[name='optin']:checked").length == 1) {
+    optin = true;
+  } else {
+    optin = false;
+  }
+  var data = {
+    "schema": "welfarm",
+    "db": {
+      "firstname": pureField($("input[name='firstname']").val()),
+      "lastname": pureField($("input[name='lastname']").val()),
+      "email": pureField($("input[name='email']").val()),
+      "optin": optin,
+      "event": "petitionporcinet"
+    },
+    "woopra" : {
+      "host": "", //TODO
+      "cookie": getCookie("wooTracker"),
+      "cv_firstname": pureField($("input[name='firstname']").val()),
+      "cv_name": pureField($("input[name='firstname']").val()) + ' ' +
+	pureField($("input[name='lastname']").val()),
+      "cv_lastname": pureField($("input[name='lastname']").val()),
+      "cv_email": pureField($("input[name='email']").val()),
+      "cv_optin": optin,
+      "event": "petitionporcinet",
+      "ce_optin": optin
+    }
+  }
+  makeCorsRequest(data);
+}
+
+function isValidEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function removeClass()
+{
+  if ($(this).attr("type") == "radio") {
+    $("input[name=" + $(this).attr("name") + "]").parent().removeClass("error");
+  } else {
+    $(this).parent().removeClass("error");
+  }
+  $(this).off("change");
+}
+
+function showError(elem) {
+  if (elem.attr("type") == "radio" || elem.attr("type") == "checkbox") {
+    $("input[name=" + elem.attr("name") + "]").parent().addClass("error");
+    $("input[name=" + elem.attr("name") + "]").on("change", removeClass);
+  } else if (elem.prop("tagName") == "SELECT") {
+    elem.parent().parent().addClass("error");
+    elem.on("change", removeClass);
+  } else {
+    elem.parent().addClass("error");
+    elem.on("change", removeClass);
+  }
+}
+
+function isValid() {
+  var status = true;
+  $(".error").removeClass("error");;
+  $("form.petitionForm input").each(function() {
+    if ($(this).attr("required") && $(this).attr != "submit") {
+      if ($(this).attr("type") == "radio" || $(this).attr("type") == "checkbox") {
+	if ($("input[name=" + $(this).attr("name") + "]:checked").length == 0) {
+	  showError($(this));
+	  status = false;
+	}
+      } else {
+	if ($(this).val() == "" || $(this).val() == null ||
+	    ($(this).attr("type") == "email" && isValidEmail($(this).val()) == false) ||
+	    ($(this).attr("name") == "phone" && isNaN($(this).val()) == true)) {
+	  showError($(this));
+	  status = false;
+	}
+      }
+    }
+  });
+  return (status);
+}
+
+$(".petitionForm").on("submit", function(e) {
+  e.preventDefault();
+  if (isValid() == true) {
+    counter(submitForm);
+  }
 });
